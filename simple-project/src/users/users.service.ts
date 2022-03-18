1e9c743b63b24b27b69df4f04dde2e5309272fce
JinHong <jin.ryu0912@gmail.com>
2022-03-18 22:00:50 +0900

    update: User

--- simple-project/src/users/users.service.ts
+++ simple-project/src/users/users.service.ts
@@ -14,14 +14,15 @@ export class UsersService {
   getUserById(id: string): User {
     return this.users.find(user) => User.id === id);
   }
-  createUsers(name: string, number: string): User {
-    const users: User = {
-      id: uuid, //해당 부분에서 순서를 자동으로 부여 및 import 수동으로 등록
-      name: name,
-      number: number,
-      status: UsersStatus.PUBLIC,
-    };
-    this.users.push(users);
-    return users;
-  }
+  
+  // createUsers(name: string, number: string): User {
+  //   const users: User = {
+  //     id: uuid, //해당 부분에서 순서를 자동으로 부여 및 import 수동으로 등록
+  //     name: name,
+  //     number: number,
+  //     status: UsersStatus.PUBLIC,
+  //   };
+  //   this.users.push(users);
+  //   return users;
+  // }
 }
