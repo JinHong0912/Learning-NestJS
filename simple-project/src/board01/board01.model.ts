0693450660cdb3998c607c8d2599797fff9a027f
JinHong <jin.ryu0912@gmail.com>
2022-04-16 21:22:49 +0900

    add : 기본 실습 익히기

---
+++ simple-project/src/board01/board01.model.ts
@@ -0,0 +1,12 @@
+export interface Board01Model {
+  // 게시글에 대한 타입을 정의
+  id: string; // 식별자
+  title: string; // 제목
+  description: string; // 내용
+  //description: string // 내용
+  status: BoardStatus; // 게시글 상태
+}
+export enum BoardStatus {
+  PUBLIC = 'PUBLIC', // 공개글
+  PRIVATE = 'PRIVATE', // 비공개글
+}
