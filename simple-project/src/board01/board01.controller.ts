10bbbcd4634129866381e4347d052f11d582e97e
JinHong <jin.ryu0912@gmail.com>
2022-04-23 21:28:29 +0900

    delete : Tast 정리

--- simple-project/src/board01/board01.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('board01')
-export class Board01Controller {
-  @Get()
-  getBoard01All(): string {
-    return 'show board01 Test';
-  }
-}
