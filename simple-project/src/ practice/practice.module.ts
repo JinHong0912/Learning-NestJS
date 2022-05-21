6d3a0ac6b73dd6135caa3862abd08851a98dfa11
JinHong <jin.ryu0912@gmail.com>
2022-05-21 21:53:48 +0900

    delete : 프로젝트 정리

--- simple-project/src/ practice/practice.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PracticeController } from './practice.controller';
-import { PracticeService } from './practice.service';
-
-@Module({
-  controllers: [PracticeController],
-  providers: [PracticeService],
-})
-export class PracticeModule {}
